df780c90f53fd5ecc94b57cbce58420a2c67f9e2
habensimanjuntak <haben.simanjuntak@gmail.com>
2021-06-20 09:14:46 +0700

    updated App.js and App.test.js

--- src/App.js
+++ src/App.js
@@ -7,9 +7,16 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         I was changed on the Feature branch updated
+          I was changed here
         </p>
-        
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn
+        </a>
       </header>
     </div>
   );
