8535e8fbe3330cc072eec1ffb736ed50ec40f773
habensimanjuntak <haben.simanjuntak@gmail.com>
2021-06-20 09:03:21 +0700

    Changed text in react

--- src/App.js
+++ src/App.js
@@ -7,16 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         I was changed on the Feature branch updated
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          I was changed on the Feature branch updated
-        </a>
+        
       </header>
     </div>
   );
